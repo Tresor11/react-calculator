23dbd5e6c493aa1376067118ed7c4c065a087562
Tresor11 <tresor.moise2001@gmail.com>
2020-04-30 09:22:43 +0200

    added eslint

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import './App.css';
 
 function App() {
   return (
+    // eslint-disable-next-line react/jsx-filename-extension
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
