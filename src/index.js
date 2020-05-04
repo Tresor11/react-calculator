49d17d00ec2b9a1cd7de058300beecefd1065409
Tresor11 <tresor.moise2001@gmail.com>
2020-05-04 08:38:15 +0200

    fixed the wrong linter error one more time

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-
 import ReactDOM from 'react-dom';
-
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
