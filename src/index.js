955d7fc39a42ea6635094b278a3606e991e8f151
Tresor11 <tresor.moise2001@gmail.com>
2020-05-04 08:30:25 +0200

    fixed linter errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+
 import ReactDOM from 'react-dom';
+
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
