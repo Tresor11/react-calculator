6057dfe32897cb4f537121c7e4cec012e98e29c6
Tresor11 <tresor.moise2001@gmail.com>
2020-05-04 15:59:21 +0200

    updated the button props

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,11 +4,20 @@ import PropTypes from 'prop-types';
 
 import React from 'react';
 
-export default function Button({ name, className }) {
-  return <button type="button" className={className}>{name}</button>;
+export default function Button({ name, color, wide }) {
+  const style = {
+    backgroundColor: color,
+    width: wide ? '50%' : '25%',
+  };
+  return <button type="button" style={style}>{name}</button>;
 }
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
-  className: PropTypes.string,
+  color: PropTypes.string,
+  wide: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  color: 'rgb(255, 171, 74)',
 };
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -6,33 +6,33 @@ export default function ButtonPanel() {
   return (
     <div className="btn-cont">
       <div className="btn-row">
-        <Button name="Ac" />
-        <Button name="+/-" />
-        <Button name="%" />
-        <Button name="÷" className="color" />
+        <Button name="Ac" color="rgb(226, 226, 226)" />
+        <Button name="+/-" color="rgb(226, 226, 226)" />
+        <Button name="%" color="rgb(226, 226, 226)" />
+        <Button name="÷" />
       </div>
       <div className="btn-row">
-        <Button name="7" />
-        <Button name="8" />
-        <Button name="9" />
-        <Button name="x" className="color" />
+        <Button name="7" color="rgb(226, 226, 226)" />
+        <Button name="8" color="rgb(226, 226, 226)" />
+        <Button name="9" color="rgb(226, 226, 226)" />
+        <Button name="x" />
       </div>
       <div className="btn-row">
-        <Button name="4" />
-        <Button name="5" />
-        <Button name="6" />
-        <Button name="-" className="color" />
+        <Button name="4" color="rgb(226, 226, 226)" />
+        <Button name="5" color="rgb(226, 226, 226)" />
+        <Button name="6" color="rgb(226, 226, 226)" />
+        <Button name="-" />
       </div>
       <div className="btn-row">
-        <Button name="1" />
-        <Button name="2" />
-        <Button name="3" />
-        <Button name="+" className="color" />
+        <Button name="1" color="rgb(226, 226, 226)" />
+        <Button name="2" color="rgb(226, 226, 226)" />
+        <Button name="3" color="rgb(226, 226, 226)" />
+        <Button name="+" />
       </div>
       <div className="btn-row">
-        <Button className="wide" name="0" />
-        <Button name="." />
-        <Button name="=" className="color" />
+        <Button wide name="0" color="rgb(226, 226, 226)" />
+        <Button name="." color="rgb(226, 226, 226)" />
+        <Button name="=" />
       </div>
     </div>
   );
